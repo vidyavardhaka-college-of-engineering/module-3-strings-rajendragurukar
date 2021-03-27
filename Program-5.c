9fcf6ebe037813ce7a389dc9db371be3862a60a4
rajendragurukar <20ec0027@vvce.ac.in>
2021-03-27 05:03:05 +0000

    done

--- Program-5.c
+++ Program-5.c
@@ -17,7 +17,10 @@ int main()
   scanf("%s", s);
 
   while (s[c] != '\0') {
-                               //place logic to find the vowel
+  if (s[c] =='a' ||s[c]== 'A' || s[c] =='e' || s[c] ==
+  'E' || s[c] == 'i' || s[c] == 'I' || s[c] =='o' || s[c] =='O' || s[c] == 'u' || s[c] == 'U')
+  count++;
+  c++;                      //place logic to find the vowel
   }
 
   printf("Number of vowels in the string = %d", count);
